3f71f084227673693b94a61ed9722e9e994a5390
Talisa Erazo <talisaerazo@gmail.com>
2018-10-14 14:57:28 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,7 +21,7 @@ def while_iterator(number_of_times)
       while number_of_times < 7
     number_of_times += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-  end
+      end
 end
 
 def until_iterator(number_of_times)
